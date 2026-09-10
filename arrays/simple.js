f576faa2fba30d1f7a43d9fcbb801d1e99ba2fdf
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-09-10 15:54:51 +0530

    add array

---
+++ arrays/simple.js
@@ -0,0 +1,4 @@
+let numbers = [11, 656, 25, 85, 684, 656, 445, 656]
+
+const findValue = numbers.findLast((a) => a == 656)
+console.log(findValue)
---
+++ arrays/users.js
@@ -0,0 +1,40 @@
+console.time()
+
+const users = [
+    {
+        "name": "Ali",
+        "mobile": "789456123",
+        "class": "B"
+    },
+    {
+        "name": "Nadim",
+        "mobile": "789456123",
+        "class": "A"
+    },
+    {
+        "name": "Ali",
+        "mobile": "789456123",
+        "class": "C"
+    },
+]
+
+
+const search = "Nadim"
+
+console.table(users)
+
+const findUser = users.filter((user) => String(user.name).toLowerCase() == search.toLowerCase())
+
+console.table(findUser)
+
+
+// for (let index = 0; index < users.length; index++) {
+//     const element = users[index];
+//     console.log("name:", element.name)
+//     console.log("mobile:", element.mobile)
+//     console.log("class:", element.class)
+// }
+
+
+
+console.timeLog()
