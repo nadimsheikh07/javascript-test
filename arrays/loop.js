35fc67e682509c88c10e2dccfb79384e36974948
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-09-17 15:55:28 +0530

    update loop

--- arrays/loop.js
+++ arrays/loop.js
@@ -2,13 +2,18 @@ import { UserData } from "../data/users.js";
 
 const data = UserData.map((user) => {
     return user
-})
+}).filter((user) => user.isActive)
 
 console.log(data)
 
+let newUsers = []
 UserData.forEach((user) => {
-    console.log(user)
+    if (user.isActive) {
+        newUsers.push(user)
+    }
 })
+
+console.log(newUsers)
 
 for (let index = 0; index < UserData.length; index++) {
     const element = UserData[index];
